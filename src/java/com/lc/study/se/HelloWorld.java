162d4be0635ae241066063fd85cc72800b823f11
DOFORME <13135541390@163.com>
2018-09-13 17:27:46 +0800

    reset project

--- src/java/com/lc/study/se/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.lc.study.se;
-
-public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("hello");
-        System.out.println("an other test");
-    }
-}
